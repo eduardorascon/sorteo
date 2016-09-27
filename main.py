84461b307783f9e51b62fd4855b4df4ca0eeeff5
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-09-27 18:00:28 -0500

    Exchange code for access token added. App Secret is left out.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import webapp2
+import urllib2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -6,12 +7,19 @@ class MainHandler(webapp2.RequestHandler):
 
 class FbHandler(webapp2.RequestHandler):
 	def get(self):
+		access_token = self.request.get('access_token')
+		
+		if not access_token == '':
+			self.redirect('../')
+
 		code = self.request.get('code')
+		
 		if code == '':
 			self.redirect('https://www.facebook.com/dialog/oauth?client_id=652896994869977&redirect_uri=https://sorteoiphone7.appspot.com/fb')
 		else:
-			self.response.write('code: ' + code)
-
+			response = urllib2.urlopen(str('https://graph.facebook.com/v2.7/oauth/access_token?client_id=652896994869977&redirect_uri=https://sorteoiphone7.appspot.com/fb&client_secret={app-secret}&code=' + code))
+			html = response.read()
+			self.response.write(html)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
