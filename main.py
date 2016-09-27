899bc5acf7ff97edf7c412d6cb09eb467f9266e6
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-09-27 17:15:32 -0500

    FbHandler added.

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class FbHandler(webapp2.RequestHandler):
+	def get(self):
+		code = self.request.get('code')
+		if code == '':
+			self.redirect('https://www.facebook.com/dialog/oauth?client_id=652896994869977&redirect_uri=https://sorteoiphone7.appspot.com/fb')
+		else:
+			self.response.write('code: ' + code)
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/fb', FbHandler),
 ], debug=True)
