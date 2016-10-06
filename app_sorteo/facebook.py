e1cc6e3efcc1346b18cf2adb4fd61821522c7915
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-10-06 13:53:41 -0500

    Better login done.

--- app_sorteo/facebook.py
+++ app_sorteo/facebook.py
@@ -1,5 +1,6 @@
 import webapp2
 import urllib2
+import json
 
 class FacebookHandler(webapp2.RequestHandler):
     client_id = '652896994869977'
@@ -7,18 +8,22 @@ class FacebookHandler(webapp2.RequestHandler):
 
     def get(self):
         code = self.request.get('code')
-        
-        if not code == '':
+        has_code = code != ''
+        access_token = ''
+
+        if has_code:
             params = (FacebookHandler.client_id, FacebookHandler.base_url, '{app-secret}', code)
             url = 'https://graph.facebook.com/v2.7/oauth/access_token?client_id=%s&redirect_uri=%s&client_secret=%s&code=%s' % params
             response = urllib2.urlopen(url)
-            html = response.read()
-            self.response.write(html)
+            json_reponse = json.load(response)
+            access_token = json_reponse['access_token']
+        else:
+            self.redirect('/')
 
-        access_token = self.request.get('access_token')
-        
-        if not access_token == '':
-            self.redirect('../')
+        has_access_token = access_token != ''
+
+        if has_access_token:
+            self.redirect('/')
 
     def post(self):
         params = (FacebookHandler.client_id, FacebookHandler.base_url)
