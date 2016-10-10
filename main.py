708a98b320b206a7ebd92549a0d3c972538953b3
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-10-10 14:06:36 -0500

    A configuration dictionary is needed for sessions to work.

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from app_sorteo.default import DefaultHandler
 from app_sorteo.facebook import FacebookHandler
 import webapp2
 
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'my-super-secret-key',
+}
+
 app = webapp2.WSGIApplication([('/', DefaultHandler),
-    ('/fb', FacebookHandler),], debug=True)
+    ('/fb', FacebookHandler),], config=config, debug=True)
