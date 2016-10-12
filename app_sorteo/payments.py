f8c64528324401a7b3113a3b62635e3664628e7d
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-10-12 16:32:50 -0500

    JavaScript function sends credit card tokenized data for the server to handle.

--- app_sorteo/payments.py
+++ app_sorteo/payments.py
@@ -4,6 +4,7 @@ import stripe
 import base
 
 class PaymentsHandler(base.BaseHandler):
+	#this key needs to change when released.
 	stripe.api_key = 'pk_test_8dd3LPVANWkLgE2WDXIcXLfw'
 	def post(self):
 		token = self.request.get('stripeToken')
