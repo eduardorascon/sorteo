6d390e370ecefb1ab871b49146631e3d28111722
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-10-13 18:21:45 -0500

    Request made to stripe is working.

--- app_sorteo/payments.py
+++ app_sorteo/payments.py
@@ -6,16 +6,17 @@ import base
 class PaymentsHandler(base.BaseHandler):
 	#this key needs to change when released.
 	stripe.api_key = 'sk_test_IHNilUCltlHhK0M4NZFV8Z1i'
+
 	def post(self):
 		token = self.request.get('stripeToken')
-		self.response.write('token: ' + token.id + '<br />')
+		self.response.write('token: ' + token + '<br />')
 
 		try:
 			charge = stripe.Charge.create(
 				amount = 1000,
 				currency = "MXN",
-				source = token.id,
-				descripcion = "Example charge"
+				source = token,
+				description = "Example charge"
 			)
 		except stripe.error.CardError as e:
 			self.response.write('error: ' + e)
