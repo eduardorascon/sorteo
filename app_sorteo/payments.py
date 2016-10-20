881299896fe2cca02e51783e39f73618ad51d4ef
Eduardo Rascon <eduardo.rascon@gmail.com>
2016-10-20 12:29:31 -0500

    Session validation added.

--- app_sorteo/payments.py
+++ app_sorteo/payments.py
@@ -8,13 +8,16 @@ class PaymentsHandler(base.BaseHandler):
 	stripe.api_key = 'sk_test_IHNilUCltlHhK0M4NZFV8Z1i'
 
 	def post(self):
+		#Check if user is logged in
+		if self.session.get('access_token') == None:
+			raise ValueError('Usuario no válido')
+
 		#We need to make get the correct charge amount.
 		ticketType =  self.request.get('ticketType')
-		#
 		if ticketType not in 'BP':
 			raise ValueError('Boleto no disponible.')
 
-		amount =  4900 if ticketType == 'B' else 7900,
+		amount = 4900 if ticketType == 'B' else 7900,
 
 		token = self.request.get('stripeToken')
 		self.response.write('token: ' + token + '<br />')
